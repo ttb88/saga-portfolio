e9cc913cd09ce8c139d7cf310e493712de021d15
Tony Braasch <tonybraasch@gmail.com>
2019-03-10 12:22:51 -0500

    add drop down options to nav bar

--- src/components/ProjectPage/ProjectNavBar.js
+++ src/components/ProjectPage/ProjectNavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { HashRouter as Link } from 'react-router-dom';
 import '../ProjectPage/ProjectNavBar.css';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -7,6 +8,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 
@@ -21,6 +24,10 @@ const styles = {
         marginLeft: -12,
         marginRight: 20,
     },
+    link: {
+        textDecoration: 'none',
+        color: 'inherit',
+    }
 };
 
 
@@ -28,15 +35,39 @@ const styles = {
 
 
 class ProjectNavBar extends Component {
+
+    state = {
+        anchorEl: null,
+    };
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+    
+    
+    
     render() {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
                 <AppBar position="static" color="primary" className={classes.root}>
                     <Toolbar>
-                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                        <IconButton onClick={this.handleClick} className={classes.menuButton} color="inherit" aria-label="Menu">
                             <MenuIcon />
                         </IconButton>
+                        <Menu
+                            id="menu"
+                            anchorEl={this.state.anchorEl}
+                            open={Boolean(this.state.anchorEl)}
+                            onClose={this.handleClose}
+                        >
+                            <MenuItem onClick={this.handleClose}><Link className={classes.link} to="/">Home</Link></MenuItem>
+                            <MenuItem onClick={this.handleClose}><Link className={classes.link} to="/admin">Admin</Link></MenuItem>
+                        </Menu>
                         <Typography variant="h6" color="inherit" className={classes.grow}>
                            My Github Portfoilio
                         </Typography>
